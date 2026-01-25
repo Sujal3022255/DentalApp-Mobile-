314dff9b91484ecd7953c17ad82be8b475ccc851
Sujal3022255 <sujalpurbey65@gmail.com>
2026-01-25 09:08:25 +0545

    Add Android test dependencies for Espresso and test runner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,10 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
     testImplementation("org.mockito:mockito-core:5.11.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 
 
     // Coroutines
